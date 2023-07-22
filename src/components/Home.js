b65c52c4d48621e5cc1780037d5e14a3e690f795
M.Amin Najary <mohammadaminnajary@gmail.com>
2023-07-23 00:36:32 +0330

    Added LogoBar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Styles from '../styles/Home.module.css';
+import Logo from '../assets/Logo/CARA-small.png';
 
 const Home = () => {
     return (
         <div>
-            Hi
+            <div>
+                <div className={Styles.logobar}>
+                    <img src={Logo} alt='LOGO' />
+                    <h2>فروشگاه ملحفه و کالای خواب کارا</h2>
+                </div>
+            </div>
         </div>
     );
 };
